24a760095083d6b64a8c11219d64ea23e9a4eaa3
Grant Kyle <sgrantkyle@gmail.com>
2021-03-21 20:07:08 -0500

    final result including semantic renaming of css, mobile responsiveness, and README.md added. Also, deleted files not used in this project

--- conversion-interactive-assessment/README.md
+++ conversion-interactive-assessment/README.md
@@ -1,70 +1,26 @@
-# Getting Started with Create React App
+# Hummer Landing Page
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a landing page for a trucking business, built with the React library for a few main reasons, but mainly because it is scalable, responsive and fast and, perhaps most importantly, it is surrounded by a huge (and growing) ecosystem, making the application's potential limitless. 
 
-## Available Scripts
+The only dependencies 
+## Instructions for use
 
-In the project directory, you can run:
+- Go to this [this](https://github.com/grantkyle/CandidateAssesment/) link
+- Click on green "Code" button, ensure option for link is set to HTTPS, and copy to your clipboard
+- In your local bash, navigate to the directory where you want this project to live and run the command
+```bash
+git clone https://github.com/grantkyle/CandidateAssesment.git
+```
+- Once finished, change directory into this project and run the command 
+```bash
+npm install
+```
+- Once dependencies are finished installing, run the command 
+```bash
+npm run start
+```
+- Once "Compiled successfully!" is displayed in your bash, open your client (Chrome preferred) and go to the address localhost:3000
+- Enjoy!
 
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+### Note
+This project was built on and optimized to mockup specs for a 13-inch Macbook Pro
--- conversion-interactive-assessment/src/components/LandingPage/LandingPage.jsx
+++ conversion-interactive-assessment/src/components/LandingPage/LandingPage.jsx
@@ -5,7 +5,7 @@ import "./styles.scss";
 const LandingPage = () => {
   return (
     <div className="hummer-landing-page__container">
-      <div className="hummer-landing-page__rows hummer-landing-page__hero-row">
+      <div className="hummer-landing-page__hero-row">
         <div className="hummer-landing-page__logo-container">
           <img
             className="hummer-landing-page__logo"
@@ -24,120 +24,102 @@ const LandingPage = () => {
         </Button>
       </div>
 
-      <div className="hummer-landing-page__rows hummer-landing-page__cta">
-        <div class="row">
-          <div class="column hummer-landing-page__cta-text-container">
-            <p className="hummer-landing-page__cta-text">
-              Want To Be Home This Weekend? With Hummer's 5-Nights-Out
-              Positions, You Will Be!
+      <div className="hummer-landing-page__cta-row">
+        <div className="hummer-landing-page__cta-row-big-text-container">
+          <p className="hummer-landing-page__cta-row-big-text">
+            Want To Be Home This Weekend? With Hummer's 5-Nights-Out Positions,
+            You Will Be!
+          </p>
+        </div>
+        <div class="hummer-landing-page__cta-row-button-and-text-container">
+          <Button
+            variant="danger"
+            button
+            className="hummer-landing-page__apply-now-button"
+          >
+            APPLY NOW&nbsp;
+            <i className="fas fa-external-link-square-alt"></i>
+          </Button>
+          <Button
+            variant="secondary"
+            className=" hummer-landing-page__phone-number-button"
+          >
+            <b>832-913-1352</b>&nbsp;{" "}
+            <i className="fas fa-phone-square-alt"></i>
+          </Button>
+
+          <Button
+            variant="dark"
+            className="hummer-landing-page__search-jobs-button"
+          >
+            SEARCH JOBS BY AREA&nbsp; <i class="fas fa-search"></i>
+          </Button>
+          <div className="hummer-landing-page__apply-now-paragraph-container">
+            <p className="hummer-landing-page__apply-now-paragraph">
+              If you're ready to end the home time guessing game, then Hummer's
+              CDL-A scheduled regional 5-nights-out truck driving positions are
+              for you! Your home time is right there in the job title. These
+              scheduled regional driving positions mean you are home every
+              weekend, running only where we can be sure that you get home on
+              time (typically out mid-day Sunday and back home mid-day Friday).
+              Take a closer look at our hiring areas and see if this opportunity
+              is right for you - we'd love to have you on board!
             </p>
-          </div>
-          <div class="column hummer-landing-page__cta-apply-now-container">
-            <div class="hummer-landing-page__cta-apply-now-button-row">
-              <div className="hummer-landing-page__cta-apply-now-button-container">
-                <Button
-                  variant="danger"
-                  className="hummer-landing-page__apply-now-button"
-                  block
-                >
-                  APPLY NOW&nbsp;
-                  <i className="fas fa-external-link-square-alt"></i>
-                </Button>
-              </div>
-              <div className="hummer-landing-page__cta-phone-number-button-container">
-                <Button
-                  block
-                  variant="secondary"
-                  className="hummer-landing-page__phone-number-button"
-                >
-                  <b>(866)913-1352</b>&nbsp;{" "}
-                  <i className="fas fa-phone-square-alt"></i>
-                </Button>
-              </div>
-            </div>
-            <div class="hummer-landing-page__cta-apply-now-search-jobs-button-row">
-              <div className="hummer-landing-page__cta-search-jobs-button-container">
-                <Button
-                  variant="dark"
-                  className="hummer-landing-page__quick-form-button"
-                  block
-                >
-                  SEARCH JOBS BY AREA&nbsp; <i class="fas fa-search"></i>
-                </Button>
-              </div>
-            </div>
-            <div className="column hummer-landing-page__cta-apply-now-paragraph-row">
-              <p className="hummer-landing-page__cta-apply-now-paragraph">
-                If you're ready to end the home time guessing game, then
-                Hummer's CDL-A scheduled regional 5-nights-out truck driving
-                positions are for you! Your home time is right there in the job
-                title. These scheduled regional driving positions mean you are
-                home every weekend, running only where we can be sure that you
-                get home on time (typically out mid-day Sunday and back home
-                mid-day Friday). Take a closer look at our hiring areas and see
-                if this opportunity is right for you - we'd love to have you on
-                board!
-              </p>
-            </div>
           </div>
         </div>
       </div>
-
-      <div className="column hummer-landing-page__rows hummer-landing-page__info-row">
-        <div className="hummer-landing-page__info-row-image-row">
-          <div class="row">
-            <div class="column hummer-landing-page__info-row-image-container">
-              <img
-                className="hummer-landing-page__info-row-image"
-                src="../assets/images/trucker.jpg"
-                alt="firstImage"
-              ></img>
-            </div>
-            <div class="column hummer-landing-page__info-row-text-container">
-              <p className="hummer-landing-page__info-row-text">
-                <b>
-                  Hummer's CDL A Scheduled Regional 5 nights-out truck driver
-                  positions offer:
-                </b>
-                <br />
-                <br />
-                <ul>
-                  <li>
-                    5 Nights Out on the road, followed by 2 consecutive nights
-                    at home every week
-                  </li>
-                  <li>
-                    No guesswork - Drivers will know dispatch time before they
-                    are done for the week on Friday.
-                  </li>
-                  <li>
-                    Hiring area limited to where freight patterns allow for home
-                    time commitments to be met - ask for details!
-                  </li>
-                  <li>
-                    Starting pay based on most recent six years of work history.
-                  </li>
-                  <li>
-                    Annual pay increases through the first seven years, then
-                    again at ten years.
-                  </li>
-                  <li>
-                    Benefits, bonus programs and too many advantages to list
-                    here!
-                  </li>
-                </ul>
-              </p>
-            </div>
+      <div className="hummer-landing-page__info-row-container">
+        <div className="hummer-landing-page__info-row">
+          <div className="hummer-landing-page__info-row-image-container">
+            <img
+              className="hummer-landing-page__info-row-image"
+              src="../assets/images/trucker.jpg"
+              alt="hummer-trucker"
+            ></img>
+          </div>
+          <div class="hummer-landing-page__info-row-text-container">
+            <p className="hummer-landing-page__info-row-text">
+              <b>
+                Hummer's CDL A Scheduled Regional 5 nights-out truck driver
+                positions offer:
+              </b>
+              <br />
+              <br />
+              <ul>
+                <li>
+                  5 Nights Out on the road, followed by 2 consecutive nights at
+                  home every week
+                </li>
+                <li>
+                  No guesswork - Drivers will know dispatch time before they are
+                  done for the week on Friday.
+                </li>
+                <li>
+                  Hiring area limited to where freight patterns allow for home
+                  time commitments to be met - ask for details!
+                </li>
+                <li>
+                  Starting pay based on most recent six years of work history.
+                </li>
+                <li>
+                  Annual pay increases through the first seven years, then again
+                  at ten years.
+                </li>
+                <li>
+                  Benefits, bonus programs and too many advantages to list here!
+                </li>
+              </ul>
+            </p>
           </div>
         </div>
-        <div className="hummer-landing-page__info-row-arrow-row">
+        <div className="hummer-landing-page__info-row-arrow">
           <i
             class="fas fa-3x fa-chevron-down"
             style={{ color: "#8b0000", paddingTop: "10px" }}
           ></i>
         </div>
       </div>
-      <div className="hummer-landing-page__rows hummer-landing-page__signup-row">
+      <div className="hummer-landing-page__signup-row">
         <div className="hummer-landing-page__signup-row-cta">
           Run Hard, Get Home, Get Paid - It's That Simple At Hummer Trucking.
           Learn More Today!
@@ -145,15 +127,15 @@ const LandingPage = () => {
       </div>
       <div className="hummer-landing-page__signup-row-form-container">
         <Form>
-          <div className="d-flex flex-row">
+          <div className="hummer-landing-page__signup-row-input-cluster">
             <div className="hummer-landing-page__signup-row-input">
-              <Form.Group controlId="formBasicEmail">
+              <Form.Group controlId="firstName">
                 <Form.Control size="sm" type="text" placeholder="First Name" />
               </Form.Group>
             </div>
 
             <div className="hummer-landing-page__signup-row-input">
-              <Form.Group controlId="formBasicEmail">
+              <Form.Group controlId="phoneNumber">
                 <Form.Control
                   size="sm"
                   type="email"
@@ -161,34 +143,37 @@ const LandingPage = () => {
                 />
               </Form.Group>
             </div>
-
             <div className="hummer-landing-page__signup-row-input">
-              <Form.Group controlId="formBasicEmail">
+              <Form.Group controlId="city">
                 <Form.Control size="sm" type="email" placeholder="City" />
               </Form.Group>
             </div>
           </div>
-          <div style={{ paddingBottom: "40px" }} className="d-flex flex-row">
+          <div
+            style={{ paddingBottom: "40px" }}
+            className="hummer-landing-page__signup-row-input-cluster"
+          >
             <div className="hummer-landing-page__signup-row-input">
-              <Form.Group controlId="formBasicEmail">
+              <Form.Group controlId="lastName">
                 <Form.Control size="sm" type="email" placeholder="Last Name" />
               </Form.Group>
             </div>
 
             <div className="hummer-landing-page__signup-row-input">
-              <Form.Group controlId="formBasicEmail">
+              <Form.Group controlId="email">
                 <Form.Control size="sm" type="email" placeholder="Email" />
               </Form.Group>
             </div>
 
             <div className="hummer-landing-page__signup-row-input">
-              <Form.Group controlId="formGridState">
+              <Form.Group controlId="state">
                 <Form.Control size="sm" as="select" defaultValue="Choose...">
                   <option>State</option>
                   <option>All the states</option>
                 </Form.Control>
               </Form.Group>
               <Button
+                className="hummer-landing-page__signup-row-button"
                 style={{ float: "left", marginTop: "10px" }}
                 variant="danger"
                 type="submit"
